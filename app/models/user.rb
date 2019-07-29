9d276e97df3749fac157c2f58b302d59fc7bdc35
Ubuntu <ubuntu@ip-172-31-34-75.us-east-2.compute.internal>
2019-07-29 16:55:49 +0000

    edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ApplicationRecord
-  has_many :likes
-  has_many :posts, through: :likes
-  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
